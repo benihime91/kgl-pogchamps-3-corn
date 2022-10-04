6c47cb933b8a68d83390483bb56007f8aa623e5b
benihime91 <aburagohain160@gmail.com>
2022-10-04 19:35:20 +0530

    final list of training notebooks

--- blk_upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-~/gdrive upload /mnt/hdd1/pogchamps_corn/NB_EXP_V2_007 --recursive
-~/gdrive upload /mnt/hdd1/pogchamps_corn/NB_EXP_V2_002 --recursive
-~/gdrive upload /mnt/hdd1/pogchamps_corn/NB_EXP_V2_005 --recursive
-~/gdrive upload /mnt/hdd1/pogchamps_corn/NB_EXP_V2_008 --recursive
-~/gdrive upload /mnt/hdd1/pogchamps_corn/NB_EXP_V2_006 --recursive
-~/gdrive upload /mnt/hdd1/pogchamps_corn/NB_EXP_V2_004 --recursive
